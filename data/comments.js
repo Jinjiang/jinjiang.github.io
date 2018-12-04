428e17e8ee06cd1e9a4f3220b2580a99d4fadb12
Jinjiang <zhaojinjiang@me.com>
2018-12-05 02:44:10 +0800

    supported nested comments

--- data/comments.js
+++ data/comments.js
@@ -23,6 +23,7 @@ Object.keys(comments).forEach(id => {
     type,
     parent
   } = comments[id]
+
   const slug = getSlug(cid)
   if (!slug) {
     console.log(id, cid, author, text)
@@ -36,11 +37,26 @@ Object.keys(comments).forEach(id => {
     parent,
     author,
     url,
-    indent: 0,
     created: moment(created * 1000).format('YYYY/MM/DD hh:mm:ss'),
     text
   })
 })
 
+Object.keys(output).forEach(slug => {
+  const comments = output[slug]
+  const ids = comments.map(({ id }) => id)
+  const refs = {}
+  comments.forEach(comment => refs[comment.id] = comment)
+
+  ids.forEach(id => {
+    const comment = refs[id]
+    const parent = refs[comment.parent]
+    if (!parent) { return }
+    parent.children = parent.children || []
+    parent.children.push(comment)
+  })
+
+  output[slug] = comments.filter(({ parent }) => !parent)
+})
 
 fs.writeFileSync(`slug-comments.json`, JSON.stringify(output, null, 2))
--- scripts/comments.js
+++ scripts/comments.js
@@ -1,4 +1,34 @@
 const slugComments = require('../data/slug-comments.json')
+
 hexo.extend.helper.register('comments', function (slug) {
-  return slugComments[slug] || []
+  const result = []
+  flatten(slugComments[slug], 0, result)
+  return result
 });
+
+function flatten(comments = [], indent, result) {
+  // for each comment
+  comments.forEach(({
+    id,
+    parent,
+    author,
+    url,
+    created,
+    text,
+    children
+  }) => {
+    // push comment ifself
+    result.push({
+      id,
+      parent,
+      author,
+      url,
+      created,
+      text,
+      indent
+    })
+    // for each child of the comment
+    // flatten(child, indent + 1, result)
+    flatten(children, indent + 1, result)
+  })
+}
