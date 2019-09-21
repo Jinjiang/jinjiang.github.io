88174de91da9ee0fa195b15ea17c0e6f10bc4104
Jinjiang <zhaojinjiang@me.com>
2019-09-22 03:01:20 +0800

    updated bg images

--- themes/iamflat/source/js/script.js
+++ themes/iamflat/source/js/script.js
@@ -24,32 +24,26 @@
 (function () {
   var header = document.querySelector('#header');
   if (header) {
-    var bgImage = '/images/bg_' + Math.ceil(Math.random() * 16) + '.jpg';
+    var bgImage = '/images/bg_' + Math.ceil(Math.random() * 45) + '.jpg';
     if (window.innerWidth <= 640) {
       bgImage = bgImage.replace('.jpg', '_m.jpg');
     }
     header.style.background = '#09c none no-repeat center center';
     header.style.backgroundSize = 'cover';
-    header.style.backgroundImage = '-webkit-linear-gradient(top, rgba(0, 144, 192, 1), rgba(0, 144, 192, 0.5)), url(' + bgImage + ')';
-    header.style.backgroundImage = '-moz-linear-gradient(top, rgba(0, 144, 192, 1), rgba(0, 144, 192, 0.5)), url(' + bgImage + ')';
-    header.style.backgroundImage = '-ms-linear-gradient(top, rgba(0, 144, 192, 1), rgba(0, 144, 192, 0.5)), url(' + bgImage + ')';
-    header.style.backgroundImage = 'linear-gradient(top, rgba(0, 144, 192, 1), rgba(0, 144, 192, 0.5)), url(' + bgImage + ')';
+    header.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 144, 192, 1), rgba(0, 144, 192, 0.5)), url(' + bgImage + ')';
   }
 })();
 
 (function () {
   var footer = document.querySelector('#footer');
   if (footer) {
-    var bgImage = '/images/bg_' + Math.ceil(Math.random() * 16) + '.jpg';
+    var bgImage = '/images/bg_' + Math.ceil(Math.random() * 45) + '.jpg';
     if (window.innerWidth <= 640) {
       bgImage = bgImage.replace('.jpg', '_m.jpg');
     }
     footer.style.background = '#f60 none no-repeat center center';
     footer.style.backgroundSize = 'cover';
-    footer.style.backgroundImage = '-webkit-linear-gradient(top, rgba(255, 96, 0, 0.5), rgba(255, 96, 0, 1)), url(' + bgImage + ')';
-    footer.style.backgroundImage = '-moz-linear-gradient(top, rgba(255, 96, 0, 0.5), rgba(255, 96, 0, 1)), url(' + bgImage + ')';
-    footer.style.backgroundImage = '-ms-linear-gradient(top, rgba(255, 96, 0, 0.5), rgba(255, 96, 0, 1)), url(' + bgImage + ')';
-    footer.style.backgroundImage = 'linear-gradient(top, rgba(255, 96, 0, 0.5), rgba(255, 96, 0, 1)), url(' + bgImage + ')';
+    footer.style.backgroundImage = 'linear-gradient(to bottom, rgba(255, 96, 0, 0.5), rgba(255, 96, 0, 1)), url(' + bgImage + ')';
   }
 })();
 
