7d8e015532b8c40145003f69b9326112e3fea12f
Jinjiang <zhaojinjiang@me.com>
2021-02-07 02:33:13 +0800

    fixed the "about me" link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Jinjiang's Dev Space",
   themeConfig: {
     nav: [
-      { text: 'About Me', link: 'https://jinjiang.me/' }
+      { text: 'About Me', link: 'http://jinjiang.me/' }
     ]
   }
 }
